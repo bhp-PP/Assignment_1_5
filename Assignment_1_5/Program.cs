281646efa1dff829c18d563df8fe71e5aa196527
BentHPedersen <benthpedersen@mail.com>
2022-02-06 23:43:09 +0100

    Add project files.

---
+++ Assignment_1_5/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+
+namespace Assignment_1_5
+{
+    class Program
+    {
+        const int BUFFER_SIZE = 10;
+        const int EXIT = -1;
+
+        const int MIN_NUMBER = 1;
+        const int MAX_NUMBER = 20;
+
+        const int PRODUCER_DELAY = 500;
+        const int CONSUMER_DELAY = 1000;
+
+        static void Main()
+        {
+            ThreadSafeQueue<int> buffer = new ThreadSafeQueue<int>(BUFFER_SIZE, EXIT);
+
+            Thread producer1 = new Thread(() => Produce(buffer, MIN_NUMBER, MAX_NUMBER, PRODUCER_DELAY));
+ 
+            Thread consumer1 = new Thread(() => Consume(buffer, CONSUMER_DELAY));
+            Thread consumer2 = new Thread(() => Consume(buffer, CONSUMER_DELAY));
+            
+            producer1.Start();
+            consumer1.Start();
+            consumer2.Start();
+
+            producer1.Join();
+            buffer.Add(buffer.EXIT);
+
+            consumer1.Join();
+            consumer2.Join();
+
+            Console.WriteLine("All Done");
+        }
+
+        public static void Produce(ThreadSafeQueue<int> buffer, int minimumValue, int maximumValue, int delayInMilliSeconds)
+        {
+            for (int item = minimumValue; item <= maximumValue; item++)
+            {
+                buffer.Add(item);
+                Thread.Sleep(delayInMilliSeconds);
+            }
+            
+            Console.WriteLine("Producer Done");
+        }
+
+        public static void Consume(ThreadSafeQueue<int> buffer, int delayInMilliSeconds)
+        {
+            int number = buffer.Fetch();
+            while (number != buffer.EXIT)
+            {
+                Console.WriteLine(number);
+                Thread.Sleep(delayInMilliSeconds);
+
+                number = buffer.Fetch();
+            }
+
+            buffer.Add(buffer.EXIT);
+
+            Console.WriteLine("Consumer Done");
+        }
+    }
+}
+
---
+++ Assignment_1_5/ThreadSafeQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Threading;
+
+namespace Assignment_1_5
+{
+    public class ThreadSafeQueue<T>
+    {
+        private readonly T[] buffer;
+        private readonly int maxSize;
+        private int first;
+        private int last;
+
+        private readonly SemaphoreSlim bufferFull;
+        private readonly SemaphoreSlim bufferEmpty;
+
+        public T EXIT { get; private set; }
+        public int Count { get; private set; }
+
+        public ThreadSafeQueue(int size, T exitValue)
+        {
+            buffer = new T[maxSize = size];
+            Count = first = last = 0;
+            EXIT = exitValue;
+            bufferFull = new SemaphoreSlim(maxSize, maxSize);
+            bufferEmpty = new SemaphoreSlim(0, maxSize);
+        }
+
+        public void Add(T item)
+        {
+            bufferFull.Wait();
+            lock (this)
+            {
+                buffer[first++] = item;
+                first %= maxSize;
+                Count++;
+            }
+            bufferEmpty.Release();
+        }
+
+        public T Fetch()
+        {
+            bufferEmpty.Wait();
+            T item = default(T);
+            lock (this)
+            {
+                item = buffer[last++];
+                last %= maxSize;
+                Count--;
+            }
+            bufferFull.Release();
+            return item;
+        }
+    }
+}
